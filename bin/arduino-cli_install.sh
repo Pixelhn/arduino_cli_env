066274ae3ebda84b33a2d68e68f082268e59f9d0
rock5b_lhn <pixelhn@qq.com>
2023-05-21 22:29:53 +0800

    update install

--- bin/arduino-cli_install.sh
+++ bin/arduino-cli_install.sh
@@ -19,12 +19,20 @@ elif ( [ $ARC == "armv7l" ] ); then
 
 fi
 
-./arduino-cli version
+cd ..
+PWD=$(pwd)
+
+sed 's|'"PWD"'|'"${PWD}"'|g' .arduino15/.arduino-cli.yaml > .arduino15/arduino-cli.yaml
+
+ln -s -f $(pwd)/.arduino15 ~/.arduino15
+
+cd -
 
 ./arduino-cli completion bash > arduino-cli.sh
-
 sudo mv arduino-cli.sh /etc/bash_completion.d/
 
-arduino-cli update
-arduino-cli upgrade
-arduino-cli core install arduino:avr
+./arduino-cli update
+./arduino-cli upgrade
+./arduino-cli core install arduino:avr
+
+./arduino-cli version > ver
