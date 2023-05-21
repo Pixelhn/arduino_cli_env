aa00d7b315ca2878d58dad1a643ca7057d9cf145
rock5b_lhn <pixelhn@qq.com>
2023-05-21 21:57:19 +0800

    update env

--- env.sh
+++ env.sh
@@ -14,8 +14,11 @@ ARDUINO_TTY=$(arduino-cli board list | grep arduino)
 arr=($ARDUINO_TTY)
 IFS=' '
 
-echo ARDUINO_TTY ${arr[0]}
-echo ARDUINO_BOARD ${arr[7]}
+echo "ARDUINO_TTY       ${arr[0]}"
+echo "ARDUINO_BOARD     ${arr[7]}"
 
 export ARDUINO_TTY=${arr[0]}
 export ARDUINO_BOARD=${arr[7]}
+
+arduino-cli update
+arduino-cli upgrade
