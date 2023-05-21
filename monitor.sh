d637ef8400bc314243b80ce0d9c6460f6b038d6e
rock5b_lhn <pixelhn@qq.com>
2023-05-21 21:45:20 +0800

    add monitor

--- monitor.sh
+++ monitor.sh
@@ -6,4 +6,13 @@ if [ $1 ]; then
     BAUDRATE="--config $1"
 fi
 
-arduino-cli monitor -p $ARDUINO_TTY  $BAUDRATE
+while [ 1 ]
+do 
+    if [ -c $ARDUINO_TTY ]; then
+        arduino-cli monitor -p $ARDUINO_TTY  $BAUDRATE
+        echo disconnect $ARDUINO_TTY
+    else
+        echo do not find $ARDUINO_TTY
+    fi
+    sleep 1
+done
--- upload.sh
+++ upload.sh
@@ -3,3 +3,5 @@
 ./build.sh || exit
 
 arduino-cli upload -p $ARDUINO_TTY --fqbn $ARDUINO_BOARD  src/
+
+./monitor.sh
